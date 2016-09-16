2ff3400297f0ecd516ec52d884137649b7854c2d
crat019 <c.k.b.ratnayake@gmail.com>
2016-09-16 13:25:35 +1200

    Added Implementations to Wordmodel and Levels

--- src/models/Level.java
+++ src/models/Level.java
@@ -1,8 +1,13 @@
 package models;
 
+
+import VoxspellApp.Voxspell;
+
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
 
 /**
  * Created by edson on 15/09/16.
@@ -27,4 +32,22 @@ public class Level implements Resettable{
         }
     }
 
+    /**
+     * This method shuffles the word list and returns a selected list of words. If there are
+     * less than 10 words in the list, the list is returned and a spelling quiz can be done
+     * with less than 10 words.
+     * @return List<Word>
+     */
+   public List<Word> getWords() {
+       Collections.shuffle(_wordList);
+       if (_wordList.size() < Voxspell.COUNT) {
+           return _wordList;
+       } else {
+           List<Word> selectedWords = new ArrayList<Word>(Voxspell.COUNT);
+           for (int i = 0; i < Voxspell.COUNT; i++) {
+               selectedWords.add(_wordList.get(i));
+           }
+           return selectedWords;
+       }
+   }
 }
