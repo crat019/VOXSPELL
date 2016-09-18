fadf2d9d414c31be3c8c1e4caac963c70ddf29ce
crat019 <c.k.b.ratnayake@gmail.com>
2016-09-19 08:06:53 +1200

    This is the spelling Logic Branch WIP

--- src/VoxspellApp/InitialScene.java
+++ src/VoxspellApp/InitialScene.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import models.SpellingQuiz;
 
 /**
  * Created by edson on 15/09/16.
